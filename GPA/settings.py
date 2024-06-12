b14b1c9a4243ad74f92b06179108413a9d39dd46
George Zakhary <elprincedoca@gmail.com>
2024-06-12 23:13:53 +0300

    add allowed hosts

--- GPA/settings.py
+++ GPA/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-(lthjsuept$k$uf@008_a)-2$d28)q&5)3^b&+0qpk^ykmw2-4
 DEBUG = True
 
 ALLOWED_HOSTS = ["joethon.alwaysdata.net"]
-ALLOWED_HOSTS = ["https://joethon.alwaysdata.net/"]
 
 
 # Application definition
